c4bf6d1f4b1a8f82b186b1b18e78e43253443205
SaeedBazargan <31041761+SaeedBazargan@users.noreply.github.com>
2023-11-13 11:26:49 +0330

    Update TranslateAPI.py
    It only translates the comments of a C code file and saves the output log.

--- TranslateAPI.py
+++ TranslateAPI.py
@@ -1,7 +1,7 @@
 import re
 import translators as ts
 
-def translate_comments(input_file, output_file):
+def translate_comments(input_file, output_file, error_log):
     with open(input_file, 'r', encoding='GBK') as file:
         c_code = file.read()
 
@@ -10,8 +10,15 @@ def translate_comments(input_file, output_file):
     # Translate each comment
     translated_comments = []
     for comment in comments:
-        translated_comment = ts.google(comment)
-        translated_comments.append(translated_comment)
+        try:
+            translated_comment = ts.google(comment)
+            if translated_comment:
+                translated_comments.append(translated_comment)
+            else:
+                error_log.write(f"Translation failed for comment: {comment}\n")
+        except Exception as e:
+            error_log.write(f"Error translating comment: {e}\n")
+            translated_comments.append(comment)  # Use the original comment if translation fails
 
     # Replace original comments with translated comments
     translated_code = c_code
@@ -23,6 +30,9 @@ def translate_comments(input_file, output_file):
         output_file.write(translated_code)
 
 # Example usage
-input_file_path = r'D:\reboot.h'
-output_file_path = r'D:\Myreboot.h'
-translate_comments(input_file_path, output_file_path)
+input_file_path = r'D:\PedDrv.h'
+output_file_path = r'D:\MYPedDrv.h'
+error_log_path = r'D:\translation_errors.log'
+
+with open(error_log_path, 'w', encoding='UTF-8') as error_log:
+    translate_comments(input_file_path, output_file_path, error_log)
